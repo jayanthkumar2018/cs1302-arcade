5178d415a0ec0877c1acedb8f45fc9bc74697702
Jayanth Kumar <44531162+jayanthkumar2018@users.noreply.github.com>
2018-12-06 14:04:29 -0500

    Checkerboard class created.

--- src/main/java/cs1302/arcade/CheckersBoard.java
+++ src/main/java/cs1302/arcade/CheckersBoard.java
@@ -1,11 +1,51 @@
 package cs1302.arcade;
 
-import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseListener;
+import javafx.scene.control.Button;
 
-public class CheckersBoard extends Canvas implements ActionListener, MouseListener {
+public class CheckersBoard
+{
+    int[][] board;
 
-    Button surrenderButton;
+    public CheckersBoard()
+    {
+        board = new int[8][8];  //0 = empty, 1 = red, 2 = black, 3 = red king, 4 = black king
+        initArray();
+    }
+
+    private void initArray()
+    {
+        for(int r = 0; r < 3; r++)
+        {
+            for(int c = 0; c < 8; c++)
+            {
+                if(r % 2 == c % 2)
+                {
+                    board[r][c] = 1;
+                }
+            }
+        }
+
+        for(int r = 5; r < 8; r++)
+        {
+            for(int c = 0; c < 8; c++)
+            {
+                if(r % 2 == c % 2)
+                {
+                    board[r][c] = 2;
+                }
+            }
+        }
+    }
+
+    public int[][] getBoard()
+    {
+        return board;
+    }
+
+    public static void main(String[] args)
+    {
+        CheckersBoard board = new CheckersBoard();
+        System.out.println(board.getBoard());
+    }
 
 } //CheckersBoard
