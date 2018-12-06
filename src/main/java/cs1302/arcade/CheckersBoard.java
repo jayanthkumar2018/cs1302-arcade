65dfd17ae04001f3f257f9ecb5dd59828002acf5
CalebColburn <caleb9494@gmail.com>
2018-12-06 13:51:20 -0500

    Test

---
+++ src/main/java/cs1302/arcade/CheckersBoard.java
@@ -0,0 +1,11 @@
+package cs1302.arcade;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+
+public class CheckersBoard extends Canvas implements ActionListener, MouseListener {
+
+    Button surrenderButton;
+
+} //CheckersBoard
